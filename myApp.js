f6a5528d41d61ff2ffcd08ceee0f0505f19d9a27
davi38 <davi4alves@gmail.com>
2022-08-09 21:36:05 -0300

    done

--- myApp.js
+++ myApp.js
@@ -3,14 +3,9 @@ let app = express();
 
 console.log('Hello World')
 
-app.get("/", function (req, res) {
-    res.send("Hello Express");
-});
-
-
-/* app.get("/", (req, res) => {
+   app.get("/", (req, res) => {
    res.send("Hello Express");
- }); */
+ });
 
 
 
