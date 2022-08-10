1ee29428a73fe6e4449c169e9261150f1594c892
davi38 <davi4alves@gmail.com>
2022-08-09 22:14:11 -0300

    new if

--- myApp.js
+++ myApp.js
@@ -12,14 +12,15 @@ console.log('Hello World')
 app.use("/public", express.static(__dirname + "/public")); //middleware, devolve CSS da pasta public. app.use(path , caminho)
 
 app.get("/json", (req, res)=>{
-    res.json({
-        message: "Hello json"
-    })
-    if (process.env.MESSAGE_STYLE === "uppercase") {
-        res.json.message.toUpperCase();
-      } else {
-        res.json.message;
-      }
+    app.get("/json", (req, res) => {
+        var response = "Hello json"
+        if (process.env.MESSAGE_STYLE == "uppercase") {
+          response = response.toUpperCase();
+        }
+        res.json({
+          message: response
+        });
+      })
 })
 
 
