eb11c6e64c31c770d9142c3a9e4f4f6cc262ac0e
davi38 <davi4alves@gmail.com>
2022-09-22 15:06:15 -0300

    feat: both ends on same branch

--- src/api/index.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-const url = process.env.URL_BACKEND;
-
-export const fetchPosts = () => axios.get(url);
-export const createPost = (newPost) => axios.post(url, newPost)
