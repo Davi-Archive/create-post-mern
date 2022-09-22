d36b4bbfa196a7212761210669dce8660023c12d
davi38 <davi4alves@gmail.com>
2022-09-21 21:18:47 -0300

    feat: post feature frontend

---
+++ src/reducers/posts.js
@@ -0,0 +1,10 @@
+export default function posts(posts = [], action) {
+  switch (action.type) {
+    case "FETCH_ALL":
+      return posts;
+    case "CREATE":
+      return [...posts, action.payload];
+    default:
+      return posts;
+  }
+};
